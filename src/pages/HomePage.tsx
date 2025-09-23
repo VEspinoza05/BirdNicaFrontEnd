92219ac7229ec44a099432dcd14a79fe6290fc61
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-09-22 18:20:17 -0600

    Configure tailwindcss and shadcn

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function HomePage() {
   return (
-    <h1>HomePage</h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1>HomePage</h1>
+      <Button>Click me</Button>
+    </div>
   )
 }
 
